f5d79f230ac613f89201baa7714ea2dcff57964e
LinaC71 <Linaalosaimi155@gmail.com>
2020-09-09 20:14:27 +0300

    done

--- cw-1-1/ios cw.playground/Contents.swift
+++ cw-1-1/ios cw.playground/Contents.swift
@@ -9,3 +9,16 @@ var height = 165
 
 print("My name is \(name) and my age is \(age) my height is \(height) and i love to \(hobby1) and \(hobby2)" )
 
+let Aname = "Aliya"
+var ahobby1 = "swimming"
+var ahobby2 = "drawing"
+var aage = 15
+var aheight = 166
+
+print("My friends name is \(Aname) and her age is \(aage) her height is \(aheight) and she loves to \(ahobby1) and \(ahobby2)" )
+
+var ageDifference = age - aage
+print(ageDifference)
+
+var mutualhobbies = (hobby1 == ahobby1 || hobby1 == ahobby2 || hobby2 == ahobby2 || hobby2 == ahobby1)
+print(mutualhobbies)
