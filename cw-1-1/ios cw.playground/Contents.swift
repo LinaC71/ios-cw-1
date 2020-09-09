208ca053dc89880aa36a53c47b89e5b6ab4124d6
LinaC71 <Linaalosaimi155@gmail.com>
2020-09-09 19:08:26 +0300

    done

---
+++ cw-1-1/ios cw.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+
+
+let name = "Lina Al-Osaimi"
+var hobby1 = "playing soccer"
+var hobby2 = "drawing"
+var age = 16
+var height = 165
+
+print("My name is \(name) and my age is \(age) my height is \(height) and i love to \(hobby1) and \(hobby2)" )
+
